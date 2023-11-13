db95fdbb66e7a42059e2d35e556b942600162ba6
Dewanshu Chhatlani <devanshu.chh@gmail.com>
2023-11-14 01:16:51 +0530

    Test cases for user requests

--- tests/user.test.js
+++ tests/user.test.js
@@ -7,20 +7,48 @@ const userOne = {
   password: "Mypass@123",
 };
 
-test("Signup a new user", async () => {
+let authToken = "";
+
+test("Should signup a new user", async () => {
   await request(app).post("/users/sign_up").send(userOne).expect(201);
 });
 
-test("Valid login credentials", async () => {
-  await request(app)
+test("Should login with valid credentials", async () => {
+  const response = await request(app)
     .post("/users/login")
     .send({ email: userOne.email, password: userOne.password })
     .expect(200);
+
+  authToken = response.body.token;
 });
 
-test("Invalid login credentials", async () => {
+test("Should not login with invalid credentials", async () => {
   await request(app)
     .post("/users/login")
     .send({ email: "Mike", password: "hello" })
     .expect(400);
+});
+
+test("Should get user profile with valid token", async () => {
+  await request(app)
+    .get("/users/me")
+    .set({ Authorization: `Bearer ${authToken}` })
+    .send()
+    .expect(200);
+});
+
+test("Should not get user profile without token", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete user with valid token", async () => {
+  await request(app)
+    .delete("/users")
+    .set({ Authorization: `Bearer ${authToken}` })
+    .send()
+    .expect(200);
+});
+
+test("Should not delete user without token", async () => {
+  await request(app).delete("/users").send().expect(401);
 });
